22bcd5773a6253d23176ba1ffdf169940c726aa3
sulekha-d2013 <sulekha-d2013@gmail.com>
2024-12-03 17:35:31 -0500

    changes made from the lab environment

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,43 +27,109 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  res.send(JSON.stringify(books,null,4));
+  let myPromise = new Promise(function(myResolve, myReject) {
+    // "Producing Code" (May take some time)
+    
+      op = JSON.stringify(books,null,4);
+      if (op.length > 0){
+        myResolve(op); // when successful
+      }
+      else {
+        myReject("Error Key value reprository corrupt.");  // when error
+      }
+
+    });
+    
+    // "Consuming Code" (Must wait for a fulfilled Promise)
+    myPromise.then(
+      function(value) { res.status(200).send(value);/* code if successful */ },
+      function(error) { res.status(500).send(error);/* code if some error */ }
+    );
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    let myPromise = new Promise(function(myResolve, myReject) {
+        // "Producing Code" (May take some time)
+        
+          const isbn = req.params.isbn;
+          op=books[isbn];
+          
+          if (Object.keys(op).length > 0){
+            myResolve(op); // when successful
+          }
+          else {
+            myReject(`Book with ISBN: ${isbn} Not Found!`);  // when error
+          }
+    
+        });
+        
+        // "Consuming Code" (Must wait for a fulfilled Promise)
+        myPromise.then(
+          function(value) { res.status(200).send(value);/* code if successful */ },
+          function(error) { res.status(404).send(error);/* code if some error */ }
+        );
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-    const author = req.params.author;
-
-    op = {};
-    for ((key, value) in books) {
-        if (value["author"] == author) {
-            op[key] = value;
+    let myPromise = new Promise(function(myResolve, myReject) {
+        // "Producing Code" (May take some time)
+        const author = req.params.author;
+        op = {};
+        for (const [key, value] of Object.entries(books)) {
+            if (value["author"] == author) {
+                op[key] = value;
+            }
+    
         }
-
-    }
-    res.send(JSON.stringify(op,null,4));
+          
+        if (Object.keys(op).length > 0){
+            myResolve(op); // when successful
+        }
+        else {
+            myReject(`Book with Author:${author} Not Found!`);  // when error
+        }
+    
+        });
+        
+        // "Consuming Code" (Must wait for a fulfilled Promise)
+        myPromise.then(
+          function(value) { res.status(200).send(JSON.stringify(value,null,4));/* code if successful */ },
+          function(error) { res.status(404).send(error);/* code if some error */ }
+        );
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-
-    op = {};
-    for ((key, value) in books) {
-        if (value["title"] == title) {
-            op[key] = value;
+    let myPromise = new Promise(function(myResolve, myReject) {
+        // "Producing Code" (May take some time)
+        const title = req.params.title;
+        op = {};
+        for (const [key, value] of Object.entries(books)) {
+            if (value["title"] == title) {
+                op[key] = value;
+            }
+    
         }
-
-    }
-    res.send(JSON.stringify(op,null,4));
+          
+        if (Object.keys(op).length > 0){
+            myResolve(op); // when successful
+        }
+        else {
+            myReject(`Book with Title:${title} Not Found!`);  // when error
+        }
+    
+        });
+        
+        // "Consuming Code" (Must wait for a fulfilled Promise)
+        myPromise.then(
+          function(value) { res.status(200).send(JSON.stringify(value,null,4));/* code if successful */ },
+          function(error) { res.status(404).send(error);/* code if some error */ }
+        );
 });
 
 //  Get book review
